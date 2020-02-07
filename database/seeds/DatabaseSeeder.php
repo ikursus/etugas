8a688b3ad10922d0e38f65a7fb908915f7684744
iKursus <ikursus.com@gmail.com>
2020-02-07 10:02:14 +0800

    Update Perkara & Laporan
    Update Perkara & Laporan

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(PenempatanTableSeeder::class);
+        $this->call(PerkaraTableSeeder::class);
     }
 }
